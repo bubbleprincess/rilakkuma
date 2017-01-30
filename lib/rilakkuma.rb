3ba6c237b714ddb20a7585790ce4ecd567be6042
Tiffany Mahr <tmahr@greenhouse.io>
2017-01-30 12:11:55 -0500

    more spacing

--- lib/rilakkuma.rb
+++ lib/rilakkuma.rb
@@ -4,10 +4,10 @@ require "rilakkuma/routing"
 module Rilakkuma
   class Application
     def call(env)
-       klass, act = get_controller_and_action(env)
-       controller = klass.new(env)
-       text = controller.send(act)
-       [200, {'Content-Type' => 'text/html'},
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
+      [200, {'Content-Type' => 'text/html'},
         [text]]
     end
   end
